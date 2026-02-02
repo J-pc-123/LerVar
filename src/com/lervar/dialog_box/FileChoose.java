b81348b633b3e2565ce293653218a6849e8dff9f
J_pc <1691365315@qq.com>
2026-02-02 20:49:18 +0800

    _

--- src/com/lervar/dialog_box/FileChoose.java
+++ src/com/lervar/dialog_box/FileChoose.java
@@ -1,7 +1,6 @@
 /**
  * LerVar(v.pre-?.?_Beta?, v.release-?.?.?)
  * <p>
- *
  * @since 2025
  * Copyright (c) 2026
  * J_pc and/or his studios
--- src/com/lervar/main/Type.java
+++ src/com/lervar/main/Type.java
@@ -1,3 +1,12 @@
+/**
+ * LerVar(v.pre-?.?_Beta?, v.release-?.?.?)
+ * <p>
+ * @since 2025
+ * Copyright (c) 2026
+ * J_pc and/or his studios
+ * All rights reserved
+ */
+
 package com.lervar.main;
 
 public class Type {
