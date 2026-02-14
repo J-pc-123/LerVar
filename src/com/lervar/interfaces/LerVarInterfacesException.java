f3224aa542c2f7f49633a8bbc6a8da35772aedef
J_pc <1691365315@qq.com>
2026-02-14 19:41:17 +0800

    _

--- src/com/lervar/interfaces/LerVarInterfacesException.java
+++ src/com/lervar/interfaces/LerVarInterfacesException.java
@@ -4,6 +4,7 @@
  * @since 2025
  * Copyright (c) 2026 J_pc and/or his studios
  * SPDX-License-Identifier: MIT
+ * URL: https://github.com/J-pc-123/LerVar/blob/main/LICENSE
  */
 
 package com.lervar.interfaces;
