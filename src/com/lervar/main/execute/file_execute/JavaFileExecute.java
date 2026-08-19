ae6ce14dc423c210e6e0e5c1861002cbeeaac053
J_pc <1691365315@qq.com>
2026-08-19 14:50:42 +0800

    Haven't been tested on WindowsOS; Haven't been tested on Linux kernel

--- src/com/lervar/main/execute/file_execute/JavaFileExecute.java
+++ src/com/lervar/main/execute/file_execute/JavaFileExecute.java
@@ -23,6 +23,7 @@ import static com.lervar.main.execute.verify.file_verify.VerifyCodeCalculate.ver
 
 public class JavaFileExecute implements JavaFileExecuteInterface {
     public static String mainMethodArrayIdentifier = "arg";
+    public static int mapPointer = 479;
     public static void javaFileExecuteOfConvert() throws Exception {
         if (!runnable) {
             return;
@@ -30,10 +31,24 @@ public class JavaFileExecute implements JavaFileExecuteInterface {
         FileExecute._LerVarFileHeadWriter();
         StringBuilder stringBuilder = new StringBuilder();
         boolean isText = false;
+        long l = 0;
         try (RandomAccessFile randomAccessFile = new RandomAccessFile(filePath, "r")) {
-            for (long i = 0; i <= Files.size(Path.of(filePath)); i++) {
-                randomAccessFile.seek(i);
-                
+            for (; l <= Files.size(Path.of(filePath)); l++) {
+                randomAccessFile.seek(l);
+                if (randomAccessFile.read() == '\"' && !isText) {
+                    isText = true;
+                } else if (randomAccessFile.read() == '\"' && isText) {
+                    isText = false;
+                } else if (isText) {
+                    //
+                    javaFileContentMap[mapPointer][1] = stringBuilder.toString();
+                    --mapPointer;
+                } else {
+                    while (!isText && !(randomAccessFile.read() == '\n' || randomAccessFile.read() == '\r' || randomAccessFile.read() == '\t' || randomAccessFile.read() == ' ')) {
+                        stringBuilder.append(randomAccessFile.read());
+                    }
+                }
+                if (true) {}
             }
         }
         
@@ -43,6 +58,9 @@ public class JavaFileExecute implements JavaFileExecuteInterface {
                 case 0x01:
                     fileOutputStream.writeLong(verifyCalculator(0, String.valueOf(_LerVarfile)));
                 case 0x02:
+                    fileOutputStream.writeLong(verifyCalculator(1, String.valueOf(_LerVarfile)));
+                case 0x03:
+                    fileOutputStream.writeLong(verifyCalculator(2, String.valueOf(_LerVarfile)));
                 }
             }
         }
